fixed rotation and movement with np arrays

player.py
@@ -3,12 +3,12 @@ from math import cos, sin
 
 #Player Eye for viewing and moving around 3D world
 class Player:
-    def __init__(self, game, screen, c=np.array([0, 10, -100]), theta=np.array([0,0,0])):
+    def __init__(self, game, screen, c=None, theta=None):
         self.game = game
         self.screen, self.screen_width, self.screen_height = screen
 
-        self.c = c
-        self.theta = theta
+        self.c = c if c is not None else np.array([0.0, 30, -100])
+        self.theta = theta if theta is not None else np.array([0.0, 0.0, 0.0])
         self.speed = 0.5
         self.look_speed = 0.03
         return
@@ -18,24 +18,16 @@ class Player:
 
         #4-Directional movement
         if self.game.key.get_pressed()[self.game.K_a]:
-            print(self.c)
             self.c[0] += self.speed * (sin(self.theta[1] - np.pi / 2));
-            self.c[1] = self.c[1];
             self.c[2] += self.speed * cos(self.theta[1] - np.pi / 2);
         if self.game.key.get_pressed()[self.game.K_d]:
-            print(self.c)
             self.c[0] -= self.speed * (sin(self.theta[1] - np.pi / 2));
-            self.c[1] = self.c[1];
             self.c[2] -= self.speed * cos(self.theta[1] - np.pi / 2);
         if self.game.key.get_pressed()[self.game.K_s]:
-            print(self.c)
             self.c[0] -= self.speed * sin(self.theta[1]);
-            self.c[1] = self.c[1];
             self.c[2] -= self.speed * cos(self.theta[1]);
         if self.game.key.get_pressed()[self.game.K_w]:
-            print(self.c)
             self.c[0] += self.speed * sin(self.theta[1]);
-            self.c[1] = self.c[1];
             self.c[2] += self.speed * cos(self.theta[1]);
 
         # Pitch, yaw, no roll camera rotations